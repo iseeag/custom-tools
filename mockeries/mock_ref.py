64a5c2eb9e0919f7488938f0685f3e4975e0b942
Maximillion <iseeag@hotmail.com>
2024-07-31 18:48:16 +0800

    rm useless import from mockeries

--- mockeries/mock_ref.py
+++ mockeries/mock_ref.py
@@ -2,8 +2,6 @@ from mockeries.mock_module import abc
 from mockeries.sub_mod.dummy import dummy_func
 from mockeries.sub_mod import \
     dummy
-from mockeries import (
-    sub_mod)
 import os
 
 if __name__ == '__main__':
